9600391f42e07a158e8da0e75ac0b75d3f718ac2
tsetan phontso <phontsotsetan@gmail.com>
2019-08-02 23:01:58 -0600

    first commit

---
+++ index.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+     <title>classes</title>
+ </head>
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ <body>
+ 	
+        <form  method="post"action="function.php">
+           
+           
+            <input type="text" name="search" required="">
+            <button type="submit">search</button>
+        </form>
+
+
+<!-- <ul class="list-group mt-5">
+  <li class="list-group-item active">Top Searched Questions</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  
+</ul>
+
+<ul class="list-group mt-5 ">
+  <li class="list-group-item active">Top Searched Topics </li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  
+</ul> -->
+
+ 
+ </body>
+ </html>
